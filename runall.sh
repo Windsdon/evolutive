3b4beaf712009b630d9517fd3b6d31a4c06c2433
mshinye <mshinye@gmail.com>
2014-09-27 11:44:00 -0300

    Add mais pop.

--- runall.sh
+++ runall.sh
@@ -1,6 +1,10 @@
 for ((1;1;1))
 do
 	player env/simple.cfg & src/a.out
+	player env/simple2.cfg & src/a.out
+	player env/simple3.cfg & src/a.out
+	player env/simple4.cfg & src/a.out
+	player env/simple5.cfg & src/a.out
 	pkill -SIGINT -f "player"
 
 done
