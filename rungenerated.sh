3c946f5f187f0eb75df722aacada643c13266636
Windsdon <Windsdon@users.noreply.github.com>
2014-10-16 05:48:57 -0300

    Fix #2

--- rungenerated.sh
+++ rungenerated.sh
@@ -1,5 +1,7 @@
-for i in $(cat generated.txt)
-do
-	player env/simple.cfg & src/a.out $i
-	pkill -SIGINT -f "player"
+while true; do
+	src/remaker ev && break;
+	for i in $(src/remaker); do
+		player env/simple.cfg & src/a.out $i
+		pkill -SIGINT -f "player"
+	done
 done
